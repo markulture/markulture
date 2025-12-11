ff60ebdd49a8018f40a9bdf8e94f4c126db6b2e3
markulture <contact.markulture@gmail.com>
2025-12-11 20:50:58 +0700

    config for deploy cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true
+    }
+  }),
   integrations: [tailwind()]
 });
